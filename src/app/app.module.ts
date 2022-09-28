e80f3ac2f042090014cae3ee4fdde869eb98766b
huevu102 <huevt.jp@gmail.com>
2022-09-28 15:22:45 +0700

    commit code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {ListComponent} from "./pages/list/list.component";
+import {BlogComponent} from "./pages/blog/blog.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {DetailComponent} from "./pages/detail/detail.component";
+import {HttpClientModule} from "@angular/common/http";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'about-us', component: AboutComponent},
+  {path: 'recipe-list', component: ListComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'contact-us', component: ContactComponent},
+  {path: 'detail', component: DetailComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ListComponent,
+    BlogComponent,
+    ContactComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
