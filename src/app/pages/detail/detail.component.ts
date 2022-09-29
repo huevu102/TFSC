a0554880b984399c25840d6cb9177cc4fdbed340
huevu102 <huevt.jp@gmail.com>
2022-09-29 08:46:34 +0700

    commit code

---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-detail',
+    templateUrl: './detail.component.html',
+    styleUrls: ['./detail.component.css']
+})
+
+export class DetailComponent extends Component{
+
+}
